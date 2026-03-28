2fab7cde5656563075dbda7c981d6a92eebe3cea
mendez-b <mendezbrainner113@gmail.com>
2026-03-28 07:45:48 +0000

    prueba

--- backend/appstarter/app/Config/Filters.php
+++ backend/appstarter/app/Config/Filters.php
@@ -50,7 +50,7 @@ class Filters extends BaseFilters
      */
     public array $required = [
         'before' => [
-            'forcehttps', // Force Global Secure Requests
+            //'forcehttps', // Force Global Secure Requests
             'pagecache',  // Web Page Caching
         ],
         'after' => [
