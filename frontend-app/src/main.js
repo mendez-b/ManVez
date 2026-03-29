2dec6198de767117173c3b453db59fb77de5d4f1
mendez-b <mendezbrainner113@gmail.com>
2026-03-29 06:21:24 -0500

    fix: clean main.js

--- frontend-app/src/main.js
+++ frontend-app/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './routers'
+import router from './routers/index.js'
 import './assets/main.css'
 
 createApp(App).use(router).mount('#app')
